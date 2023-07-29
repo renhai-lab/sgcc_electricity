361475792dd2e4bfbea3afb347f044709626821f
renhaiidea <jzw.1996@foxmail.com>
2023-07-30 01:00:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,306 @@
+<h1 align="center">国家电网电力获取</h1>
+<p align="center">
+<img src="assets/202307291641084-16906224463663.png" alt="mini-graph-card" width="400">
+</p>
+
+
+
+本应用可以帮助你将国网的电费、用电量数据接入HA，或者保存到本地。具体提供两类数据：
+
+1. 在homeassistant以实体显示：
+
+   ```
+   sensor.last_electricity_usage：最近一天用电量
+   sensor.electricity_charge_balance：预付费显示电费余额，反之显示上月应交电费
+   sensor.yearly_electricity_usage： 今年总用电量
+   sensor.yearly_electricity_charge:  今年总用电费用
+   ```
+
+1. 近三十天每日用电量数据。
+
+## 一、适用范围：
+
+适用于除南方电网覆盖省份外的用户。即除广东、广西、云南、贵州、海南等省份的用户外，均可使用本应用获取电力、电费数据。
+
+不管是通过哪种哪种安装的homeassistant，只要可以运行python，都可以采用本仓库部署。
+
+## 二、实现流程
+
+通过python实现selenium获取国家电网的数据，通过homeassistant的提供的[REST API](https://developers.home-assistant.io/docs/api/rest/)将采用POST请求将实体状态更新到homeassistant。
+
+## 三、安装
+
+### 方法一：docker镜像部署，速度快 
+1. 安装docker，方法自行百度
+
+2. 创建项目文件夹
+
+   ```bash
+   mkdir sgcc_electricity & cd sgcc_electricity 
+   ```
+
+3. 创建环境变量文件
+
+   ```bash
+   vim .env
+   ```
+
+   参考以下文件编写.env文件
+
+   ```bash
+   # 国网登录信息
+   PHONE_NUMBER="xxx" # 手机号
+   PASSWORD="xxxx" # 密码
+   
+   # 数据库配置
+   MONGO_URL="mongodb://USERNAME:PASSWORD@localhost:27017/"
+   DB_NAME="homeassistant"
+   # COLLECTION_NAME默认为electricity_daily_usage_{国网用户id}
+   
+   # homeassistant配置
+   HASS_URL="http://localhost:8123/" # homeassistant地址
+   HASS_TOKEN="token" # 长期令牌
+   
+   # selenium运行参数
+   JOB_START_TIME="07:00"
+   
+   # 以下文件不一定需要修改 单位都为秒
+   DRIVER_IMPLICITY_WAIT_TIME=60
+   RETRY_TIMES_LIMIT=5
+   LOGIN_EXPECTED_TIME=60
+   RETRY_WAIT_TIME_OFFSET_UNIT=10
+   FIRST_SLEEP_TIME=10
+   
+   # 日志级别
+   LOG_LEVEL="INFO" # DEBUG 可以查看报错信息
+   ```
+
+4. 编写docekr-compose.yml文件
+
+   ```bash
+   vim docekr-compose.yml
+   ```
+
+   填入以下内容
+
+   ```yaml
+   version: "3"
+   
+   services:
+     app:
+       env_file:
+         - .env
+       image: renhai/sgcc_electricity:latest
+       container_name: sgcc_electricity
+       network_mode: bridge
+       environment:
+         - SET_CONTAINER_TIMEZONE=true
+         - CONTAINER_TIMEZONE=Asia/Shanghai
+       restart: unless-stopped
+       command: python3 main.py
+   
+   # 默认将近30天数据写入mongo数据库，方便查询
+     mongo:
+       image: mongo:4.4.18
+       restart: always
+       container_name: mongo-for-sgcc
+       network_mode: bridge
+       environment:
+         MONGO_INITDB_ROOT_USERNAME: USERNAME # 修改为自己的用户名
+         MONGO_INITDB_ROOT_PASSWORD: PASSWORD # 修改为自己的密码
+         MONGODB_DATABASE: "homeassistant" # 修改为自己的数据库名,和.env中的数据库名一致
+         CONTAINER_TIMEZONE: Asia/Shanghai
+       volumes:
+         - ./db:/data/db
+       ports:
+         - "27017:27017"
+   ```
+
+### 方法二：本地自行构建容器
+
+1. 克隆仓库
+
+   ```bash
+   git clone https://github.com/renhaiidea/sgcc_electricity.git \
+   & cd sgcc_electricity
+   ```
+
+2. 参考sample.env编写.env文件
+
+   ```
+   cp sample.env ./env
+   ```
+
+3. 查阅docker-compose文件，默认不需要修改
+
+4. 运行
+
+   ```bash
+   docker compose up --build 
+   # 或者后台运行
+   docker compose up -d --build
+   ```
+
+### 方法三，不推荐，不安装docker，安装python环境后直接运行：
+
+克隆仓库之后,参考Dockerfile的命令，<u>自行配置安装chrome浏览器和浏览器驱动</u>，安装mongodb，将sample.env文件复制scripts文件夹，到然后运行mian.py文件。
+
+
+
+## 四、配置与使用
+
+### 1.**！！！ 第一次运行需要创建并填写.env文件，按文件说明进行填写。**
+
+### 2.（可选）填写homeassistant的配置文件
+
+由于采用REST API方式创建sensor，没有做实体注册，无法在webui里编辑。如果需要，你可以在configuration.yaml下增加如下配置后重启HA，这样你就可在webUI编辑对应的实体了，这样含有_entity后缀的实体就可以进行修改了。
+
+- 如果你有一个户号，参照以下配置：
+
+```yaml
+# Example configuration.yaml entry
+# 文件中只能有一个template
+template:
+  # 参考文档： https://www.home-assistant.io/integrations/template
+	# trigger为原始sensor，实际请使用sensor中的实体。
+  - trigger:
+      - platform: event
+        event_type: "state_changed"
+        event_data:
+          entity_id: sensor.electricity_charge_balance
+    sensor:
+      - name: electricity_charge_balance_entity
+        unique_id: electricity_charge_balance_entity
+        state: "{{ states('sensor.electricity_charge_balance') }}"
+        state_class: measurement
+        unit_of_measurement: "CNY"
+        device_class: monetary
+####################
+  - trigger:
+      - platform: event
+        event_type: "state_changed"
+        event_data:
+          entity_id: sensor.last_electricity_usage
+    sensor:
+      - name: 最近一天用电量
+        unique_id: last_electricity_usage_entity
+        state: "{{ states('sensor.last_electricity_usage') }}"
+        attributes:
+          present_date: "{{ state_attr('sensor.last_electricity_usage', 'present_date') }}"
+          last_updated: "{{ state_attr('sensor.last_electricity_usage', 'last_updated') }}"
+        state_class: total
+        unit_of_measurement: "kWh"
+        device_class: energy
+######################
+  - trigger:
+      - platform: event
+        event_type: "state_changed"
+        event_data:
+          entity_id: sensor.yearly_electricity_usage
+    sensor:
+      - name: yearly_electricity_usage_entity
+        unique_id: yearly_electricity_usage_entity
+        state: "{{ states('sensor.yearly_electricity_usage') }}"
+        state_class: total_increasing
+        unit_of_measurement: "kWh"
+        device_class: energy
+######################
+  - trigger:
+      - platform: event
+        event_type: "state_changed"
+        event_data:
+          entity_id: sensor.yearly_electricity_charge
+    sensor:
+      - name: yearly_electricity_charge_entity
+        unique_id: yearly_electricity_charge_entity
+        state: "{{ states('sensor.yearly_electricity_charge') }}"
+        state_class: total_increasing
+        unit_of_measurement: "CNY"
+        device_class: monetary
+############################################
+```
+
+- 如果你有多个户号，每个户号参照[configuration.yaml](template/configuration.yaml)配置。
+
+  > **注：如果你有一个户号，在HA里就是以上实体名；如果你有多个户号，实体名称还要加 “_户号”后缀，举例:sensor.last_electricity_usage_1234567890**
+
+### 3.（可选）ha内数据展示 [mini-graph-card](https://github.com/kalkih/mini-graph-card) 实现效果
+
+![202307291641084.png](assets%2F202307291641084.png)
+
+```yaml
+type: custom:mini-graph-card
+entities:
+  - entity: sensor.last_electricity_usage_entity
+    name: 国网查询每日用电量
+    aggregate_func: max
+group_by: date
+hour24: true
+hours_to_show: 125
+```
+
+### 4.（可选）配合用电阶梯，实现实时电价。
+
+![image-20230729172257731](assets/image-20230729172257731.png)
+
+#### 具体操作：
+
+修改homeassistant.yml文件然后重启或重载配置文件，注意当前阶梯中的sensor.yearly_electricity_usage_entity要根据你的实际清空修改。：
+
+```yaml
+# 文件中只能有一个sensor
+sensor:
+  # 实时电价
+  - platform: template #平台名称
+    sensors: #传感器列表
+      real_time_electricity_price: #实体名称：只能用小写，下划线
+        unique_id: "real_time_electricity_price" #UID（必须）
+        friendly_name:  '实时电价' #在前端显示的传感器昵称（可选)
+        unit_of_measurement: "CNY/kWh" #传感器数值的单位（可选）
+        icon_template: mdi:currency-jpy #默认图标
+        value_template: > #定义一个获取传感器状态的模板（必须）下面的6和22是指6点和22点，"1""2""3"是指阶梯123，6个价格分别是3个阶梯的峰谷价格
+          {% if now().strftime("%H")| int >= 6 and now().strftime("%H")|int < 22 and states("sensor.current_ladder")=="1" %}
+            0.617
+          {%elif now().strftime("%H")| int >= 6 and now().strftime("%H")|int < 22 and states("sensor.current_ladder")=="2" %}
+            0.677
+          {%elif now().strftime("%H")| int >= 6 and now().strftime("%H")|int < 22 and states("sensor.current_ladder")=="3" %}
+            0.977
+          {% elif states("sensor.current_ladder")=="1" %}
+            0.307
+          {% elif states("sensor.current_ladder")=="2" %}
+            0.337
+          {% elif states("sensor.current_ladder")=="3" %}
+            0.487
+          {% endif %}
+
+# 当前阶梯
+  - platform: template
+    sensors:
+      current_ladder:
+        unique_id: "current_ladder"
+        friendly_name:  '当前阶梯'
+        unit_of_measurement: "级"
+        icon_template: mdi:elevation-rise
+        value_template: > #这里是上海的三个阶梯数值，第2阶梯3120，第三阶梯4800，为了统计准确，大家可以减去现如今已经用过的总度数
+          {% if states("sensor.yearly_electricity_usage_entity") | float <= 3120 %}
+          1
+          {% elif states("sensor.yearly_electricity_usage_entity") | float >3120 and states("sensor.yearly_electricity_usage_entity") | float <= 4800 %}
+          2
+          {% else %}
+          3
+          {% endif %}
+```
+
+## 写在最后
+> ~~原作者：[https://github.com/louisslee/sgcc_electricity](https://github.com/louisslee/sgcc_electricity)~~，原始[README_origin.md](归档/README_origin.md)。
+>
+> **有人同步过：**[https://github.com/liantianji/sgcc_electricity](https://github.com/liantianji/sgcc_electricity)
+
+## 我的自定义部分包括：
+
+增加的部分：
+
+- 增加近30天每日电量写入数据库（默认mongodb）
+- 将间歇执行设置为定时执行: JOB_START_TIME，默认为"07:00”
+- 给last_daily_usage增加present_date，用来确定更新的是哪一天的电量。一般查询的日期会晚两天。
+- configuration.yaml文件修改。
---
+++ scripts/data_fetcher.py
@@ -0,0 +1,335 @@
+import logging
+import os
+import re
+import subprocess
+import time
+import traceback
+
+import ddddocr
+import dotenv
+import pymongo
+import undetected_chromedriver as uc
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from const import *
+
+
+class DataFetcher:
+
+    def __init__(self, username: str, password: str):
+        dotenv.load_dotenv()
+        self._username = username
+        self._password = password
+        self._ocr = ddddocr.DdddOcr(show_ad=False)
+        self._chromium_version = self._get_chromium_version()
+        MONGO_URL = os.getenv("MONGO_URL")
+        DB_NAME = os.getenv("DB_NAME")
+        self.client = pymongo.MongoClient(MONGO_URL)
+        self.db = self.client[DB_NAME]
+        self.DRIVER_IMPLICITY_WAIT_TIME = int(os.getenv("DRIVER_IMPLICITY_WAIT_TIME"))
+        self.RETRY_TIMES_LIMIT = int(os.getenv("RETRY_TIMES_LIMIT"))
+        self.LOGIN_EXPECTED_TIME = int(os.getenv("LOGIN_EXPECTED_TIME"))
+        self.RETRY_WAIT_TIME_OFFSET_UNIT = int(os.getenv("RETRY_WAIT_TIME_OFFSET_UNIT"))
+
+    def create_db(self, user_id):
+        collection_name = f"electricity_daily_usage_{user_id}"
+        try:
+            self.collection = self.db.create_collection(collection_name)
+            logging.info(f"集合: {collection_name} 创建成功")
+        except:
+            self.collection = self.db[collection_name]
+            logging.warning("集合已存在")
+
+        try:
+            self.collection.create_index([('date', pymongo.DESCENDING)], unique=True)
+            logging.info(f"创建索引成功")
+        except:
+            logging.warning("索引已存在")
+
+    def fetch(self):
+        """the entry, only retry logic here """
+        for retry_times in range(1, self.RETRY_TIMES_LIMIT + 1):
+            try:
+                return self._fetch()
+            except Exception as e:
+                if retry_times == self.RETRY_TIMES_LIMIT:
+                    raise e
+                traceback.print_exc()
+                logging.error(
+                    f"Webdriver quit abnormly, reason: {e}. {self.RETRY_TIMES_LIMIT - retry_times} retry times left.")
+                wait_time = retry_times * self.RETRY_WAIT_TIME_OFFSET_UNIT
+                time.sleep(wait_time)
+
+    def _fetch(self):
+        """main logic here"""
+
+        driver = self._get_webdriver()
+        logging.info("Webdriver initialized.")
+
+
+        try:
+
+            self._login(driver)
+            logging.info(f"Login successfully on {LOGIN_URL}")
+
+            user_id_list = self._get_user_ids(driver)
+            for user_id in user_id_list:
+                self.create_db(user_id)
+            logging.info(f"get all user id: {user_id_list}")
+
+            balance_list = self._get_electric_balances(driver, user_id_list)  #
+            ### get data except electricity charge balance
+            last_daily_date_list, last_daily_usage_list, yearly_charge_list, yearly_usage_list = self._get_other_data(driver, user_id_list)
+
+            driver.quit()
+
+            logging.info("Webdriver quit after fetching data successfully.")
+            logging.info("浏览器已退出")
+            return user_id_list, balance_list, last_daily_date_list, last_daily_usage_list, yearly_charge_list, yearly_usage_list
+
+        finally:
+            driver.quit()
+
+    def _get_webdriver(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--incognito')
+        chrome_options.add_argument('--window-size=4000,1600')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = uc.Chrome(driver_executable_path="/usr/bin/chromedriver", options=chrome_options, version_main=self._chromium_version)
+        driver.implicitly_wait(self.DRIVER_IMPLICITY_WAIT_TIME)
+        return driver
+
+    def _login(self, driver):
+
+        driver.get(LOGIN_URL)
+
+        # swtich to username-password login page
+        driver.find_element(By.CLASS_NAME, "user").click()
+
+        # input username and password
+        input_elements = driver.find_elements(By.CLASS_NAME, "el-input__inner")
+        input_elements[0].send_keys(self._username)
+        input_elements[1].send_keys(self._password)
+
+        captcha_element = driver.find_element(By.CLASS_NAME, "code-mask")
+
+        # sometimes ddddOCR may fail, so add retry logic)
+        for retry_times in range(1, self.RETRY_TIMES_LIMIT + 1):
+
+            img_src = captcha_element.find_element(By.TAG_NAME, "img").get_attribute("src")
+            img_base64 = img_src.replace("data:image/jpg;base64,", "")
+            orc_result = str(self._ocr.classification(ddddocr.base64_to_image(img_base64)))
+
+            if (not self._is_captcha_legal(orc_result)):
+                logging.debug(
+                    f"The captcha is illegal, which is caused by ddddocr, {self.RETRY_TIMES_LIMIT - retry_times} retry times left.")
+                WebDriverWait(driver, self.DRIVER_IMPLICITY_WAIT_TIME).until(EC.element_to_be_clickable(captcha_element))
+                driver.execute_script("arguments[0].click();", captcha_element)
+                time.sleep(2)
+                continue
+
+            input_elements[2].send_keys(orc_result)
+
+            # click login button
+            self._click_button(driver, By.CLASS_NAME, "el-button.el-button--primary")
+            try:
+                return WebDriverWait(driver, self.LOGIN_EXPECTED_TIME).until(EC.url_changes(LOGIN_URL))
+            except:
+                logging.debug(
+                    f"Login failed, maybe caused by invalid captcha, {self.RETRY_TIMES_LIMIT - retry_times} retry times left.")
+
+        raise Exception(
+            "Login failed, maybe caused by 1.incorrect phone_number and password, please double check. or 2. network, please mnodify LOGIN_EXPECTED_TIME in const.py and rebuild.")
+
+    def _get_electric_balances(self, driver, user_id_list):
+
+        balance_list = []
+
+        # switch to electricity charge balance page
+        driver.get(BALANCE_URL)
+
+        # get electricity charge balance for each user id
+        for i in range(1, len(user_id_list) + 1):
+            balance = self._get_eletric_balance(driver)
+            if (balance is None):
+                logging.info(f"Get electricity charge balance for {user_id_list[i - 1]} failed, Pass.")
+            else:
+                logging.info(
+                    f"Get electricity charge balance for {user_id_list[i - 1]} successfully, balance is {balance} CNY.")
+            balance_list.append(balance)
+
+            # swtich to next userid
+            if (i != len(user_id_list)):
+                self._click_button(driver, By.CLASS_NAME, "el-input__inner")
+                self._click_button(driver, By.XPATH,
+                                   f"//ul[@class='el-scrollbar__view el-select-dropdown__list']/li[{i + 1}]")
+
+        return balance_list
+
+    def _get_other_data(self, driver, user_id_list):
+        last_daily_date_list = []
+        last_daily_usage_list = []
+        yearly_usage_list = []
+        yearly_charge_list = []
+
+        # swithc to electricity usage page
+        driver.get(ELECTRIC_USAGE_URL)
+
+        # get data for each user id
+        for i in range(1, len(user_id_list) + 1):
+
+            yearly_usage, yearly_charge = self._get_yearly_data(driver)
+
+            if yearly_usage is None:
+                logging.error(f"Get year power usage for {user_id_list[i - 1]} failed, pass")
+            else:
+                logging.info(
+                    f"Get year power usage for {user_id_list[i - 1]} successfully, usage is {yearly_usage} kwh")
+            if yearly_charge is None:
+                logging.error(f"Get year power charge for {user_id_list[i - 1]} failed, pass")
+            else:
+                logging.info(
+                    f"Get year power charge for {user_id_list[i - 1]} successfully, yealrly charge is {yearly_charge} CNY")
+
+            # get yesterday usage
+            last_daily_datetime, last_daily_usage = self._get_yesterday_usage(driver)
+
+            # 新增储存30天用电量
+            self._save_daily_usage(driver)
+
+            if last_daily_usage is None:
+                logging.error(f"Get daily power consumption for {user_id_list[i - 1]} failed, pass")
+            else:
+                logging.info(
+                    f"Get daily power consumption for {user_id_list[i - 1]} successfully, , {last_daily_datetime} usage is {last_daily_usage} kwh.")
+
+            last_daily_date_list.append(last_daily_datetime)
+            last_daily_usage_list.append(last_daily_usage)
+            yearly_charge_list.append(yearly_charge)
+            yearly_usage_list.append(yearly_usage)
+
+            # switch to next user id
+            if i != len(user_id_list):
+                self._click_button(driver, By.CLASS_NAME, "el-input.el-input--suffix")
+                self._click_button(driver, By.XPATH,
+                                   f"//body/div[@class='el-select-dropdown el-popper']//ul[@class='el-scrollbar__view el-select-dropdown__list']/li[{i + 1}]")
+
+        return last_daily_date_list, last_daily_usage_list, yearly_charge_list, yearly_usage_list
+
+    def _get_user_ids(self, driver):
+
+        # click roll down button for user id
+        self._click_button(driver, By.XPATH, "//div[@class='el-dropdown']/span")
+        # wait for roll down menu displayed
+        target = driver.find_element(By.CLASS_NAME, "el-dropdown-menu.el-popper").find_element(By.TAG_NAME, "li")
+        WebDriverWait(driver, self.DRIVER_IMPLICITY_WAIT_TIME).until(EC.visibility_of(target))
+        WebDriverWait(driver, self.DRIVER_IMPLICITY_WAIT_TIME).until(
+            EC.text_to_be_present_in_element((By.XPATH, "//ul[@class='el-dropdown-menu el-popper']/li"), ":"))
+
+        # get user id one by one
+        userid_elements = driver.find_element(By.CLASS_NAME, "el-dropdown-menu.el-popper").find_elements(By.TAG_NAME,
+                                                                                                         "li")
+        userid_list = []
+        for element in userid_elements:
+            userid_list.append(re.findall("[0-9]+", element.text)[-1])
+        return userid_list
+
+    def _get_eletric_balance(self, driver):
+        try:
+            balance = driver.find_element(By.CLASS_NAME, "num").text
+            return float(balance)
+        except:
+            return None
+
+    def _get_yearly_data(self, driver):
+
+        try:
+            self._click_button(driver, By.XPATH, "//div[@class='el-tabs__nav is-top']/div[@id='tab-first']")
+
+            # wait for data displayed
+            target = driver.find_element(By.CLASS_NAME, "total")
+            WebDriverWait(driver, self.DRIVER_IMPLICITY_WAIT_TIME).until(EC.visibility_of(target))
+        except:
+            return None, None
+
+        # get data
+        try:
+            yearly_usage = driver.find_element(By.XPATH, "//ul[@class='total']/li[1]/span").text
+
+        except:
+            yearly_usage = None
+
+        try:
+            yearly_charge = driver.find_element(By.XPATH, "//ul[@class='total']/li[2]/span").text
+        except:
+            yearly_charge = None
+
+        return yearly_usage, yearly_charge
+
+    def _get_yesterday_usage(self, driver):
+        """获取最近一次用电量"""
+        try:
+            # 点击日用电量
+            self._click_button(driver, By.XPATH, "//div[@class='el-tabs__nav is-top']/div[@id='tab-second']")
+            # wait for data displayed
+            usage_element = driver.find_element(By.XPATH,
+                                                "//div[@class='el-tab-pane dayd']//div[@class='el-table__body-wrapper is-scrolling-none']/table/tbody/tr[1]/td[2]/div")
+            WebDriverWait(driver, self.DRIVER_IMPLICITY_WAIT_TIME).until(EC.visibility_of(usage_element)) # 等待用电量出现
+
+            # 增加是哪一天
+            date_element = driver.find_element(By.XPATH,
+                                               "//div[@class='el-tab-pane dayd']//div[@class='el-table__body-wrapper is-scrolling-none']/table/tbody/tr[1]/td[1]/div")
+            last_daily_date = date_element.text # 获取最近一次用电量的日期
+            return last_daily_date, float(usage_element.text)
+        except:
+            return None
+
+    # 增加储存30天用电量的到mongodb的函数
+    def _save_daily_usage(self, driver):
+        # 30天用电量的按钮
+        self._click_button(driver, By.XPATH, "//*[@id='pane-second']/div[1]/label[2]/span[2]")
+        # 等待30天用电量的数据出现
+        usage_element = driver.find_element(By.XPATH,
+                                            "//div[@class='el-tab-pane dayd']//div[@class='el-table__body-wrapper is-scrolling-none']/table/tbody/tr[1]/td[2]/div")
+        WebDriverWait(driver, self.DRIVER_IMPLICITY_WAIT_TIME).until(EC.visibility_of(usage_element))
+        # 30天用电量的数据
+        days_element = driver.find_elements(By.XPATH,
+                                            "//*[@id='pane-second']/div[2]/div[2]/div[1]/div[3]/table/tbody/tr")  # 30天的值 列表 2023-05-0511.98
+
+        for i in days_element:
+            day = i.find_element(By.XPATH, "td[1]/div").text
+            usage = i.find_element(By.XPATH, "td[2]/div").text
+            dic = {'date': day, 'usage': float(usage)}
+            try:
+                self.collection.insert_one(dic)
+                logging.info(f"{day}的用电量{usage}KW已经成功存入数据库")
+            except:
+                logging.debug(f"{day}的用电量存入数据库失败,可能已经存在")
+
+    @staticmethod
+    def _click_button(driver, button_search_type, button_search_key):
+        '''wrapped click function, click only when the element is clickable'''
+        click_element = driver.find_element(button_search_type, button_search_key)
+        WebDriverWait(driver, int(os.getenv("DRIVER_IMPLICITY_WAIT_TIME"))).until(EC.element_to_be_clickable(click_element))
+        driver.execute_script("arguments[0].click();", click_element)
+
+    @staticmethod
+    def _is_captcha_legal(captcha):
+        ''' check the ddddocr result, justify whether it's legal'''
+        if (len(captcha) != 4):
+            return False
+        for s in captcha:
+            if (not s.isalpha() and not s.isdigit()):
+                return False
+        return True
+
+    @staticmethod
+    def _get_chromium_version():
+        result = str(subprocess.check_output(["chromium", "--product-version"]))
+        return re.findall(r"(\d*)\.", result)[0]
